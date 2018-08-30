067707ed216ef16255316e3c40afec4958ea1a5b
xujunyu <junyu.x@shulidata.com>
2018-08-30 16:17:38 +0800

    reformat

--- src/main/java/storm/mongo/novel/ContentBolt.java
+++ src/main/java/storm/mongo/novel/ContentBolt.java
@@ -8,7 +8,6 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,13 +27,11 @@ public class ContentBolt extends BaseRichBolt {
         String content = rawContent.replace("\\n", "").replace("\\t", "");
         List<String> list = Splitter.on("\n").trimResults().omitEmptyStrings().splitToList(content);
 
-        Map<String, String> map = new HashMap<>();
-
-        StringBuilder builder = new StringBuilder();
-
         boolean start = false;
         boolean end = false;
         boolean infoParsed = false;
+
+        StringBuilder builder = new StringBuilder();
         for (String line : list) {
             if (!start && line.contains("---|---|---")) {
                 start = true;
